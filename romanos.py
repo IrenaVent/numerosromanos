e5251ac1de65e33bd037978a666e722754c510d1
irena <irena.vent@gmail.com>
2021-07-21 00:21:27 +0200

    Restas y test

--- romanos.py
+++ romanos.py
@@ -4,9 +4,34 @@ simbolos = {
     "centenas": ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],
     "millares": ["","M", "MM", "MMM"] }
 
+digitos_romanos = {
+    "I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+
+def a_numero (cadena):
+    acumulador = 0
+    valor_ant = 0
+    for caracter in cadena:
+        valor = digitos_romanos[caracter]
+        if valor > valor_ant:
+            if valor_ant in (5,50,500):
+                raise ValueError("No se pueden restrar V, L o D")
+            
+            if valor_ant > 0 or valor > 10 * valor_ant:
+                raise ValueError("No se pueden restas entre digitos 10veces mayores")
+            
+            acumulador = acumulador - valor_ant
+            acumulador = acumulador + valor - valor_ant
+        else:
+            acumulador += valor
+        
+        valor_ant = valor
+
+    return acumulador
+
 def validar (n):                                           
     if not isinstance(n, int): # para comprobar el dato, "n" el dato que ponemos e "int" que debe comprobar
-        raise ValueError("{} debe ser un entero".format(n)) # lanzamos una excepcion
+        raise ValueError("{} debe ser un entero".format(n)) 
+        # lanzamos una excepcion
     if 0 > n or n > 3999:
         raise ValueError("{} debe estar entre 0 y 3999".format(n))
 
@@ -34,8 +59,14 @@ def a_romano(n):
     if len(c) >= 4:
        millares = int(c[-4])
 
-    # componentes = (millares, centenas, decenas, unidades)
+    # otra forma de solventar is "if"
+    # c= {:04d}.format(n)
+    # unidades = int(c[-1])
+    # decenas = int(c[-2])
+    # centenas = int(c[-3])
+    # millares = int(c[-4])
 
     return simbolos["millares"][millares] + simbolos["centenas"][centenas] + simbolos["decenas"][decenas] + simbolos["unidades"][unidades]
+
 
   
